c31c3e943231e9a28ae54ef8cdf5eb134250f214
unknown <sriyogesh94@gmail.com>
2017-10-25 18:26:30 +0530

    refactor : Clone udacity reactnd-contacts-app repo

--- src/App.js
+++ src/App.js
@@ -1,36 +1,53 @@
 import React, { Component } from 'react';
-
-class ContactList extends React.Component {
-  render () {
-    const people = this.props.contacts
-
-    return (
-      <ol>
-        {people.map(person => (
-          <li key={person.name}>{person.name}</li>
-        ))}
-      </ol>
-    )
-
-  }
-}
+import { Route } from 'react-router-dom'
+import ListContacts from './ListContacts'
+import CreateContact from './CreateContact'
+import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
+  state = {
+    contacts: []
+  }
+  componentDidMount() {
+    ContactsAPI.getAll().then((contacts) => {
+      this.setState({ contacts })
+    })
+  }
+  removeContact = (contact) => {
+    this.setState((state) => ({
+      contacts: state.contacts.filter((c) => c.id !== contact.id)
+    }))
+
+    ContactsAPI.remove(contact)
+  }
+
+  createContact(contact) {
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(state => ({
+        contacts: state.contacts.concat([ contact ])
+      }))
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <ContactList contacts={[
-          { name : 'Yogesh' },
-          { name : 'Kapil' },
-          { name : 'Prateek' }
-        ]}/>
-        <ContactList contacts={[
-          { name : 'Sri' },
-          { name : 'Jain' },
-          { name : 'Raj' }
-        ]}/>
+      <div>
+        <Route exact path='/' render={() => (
+          <ListContacts
+            onDeleteContact={this.removeContact}
+            contacts={this.state.contacts}
+          />
+        )}/>
+        <Route path='/create' render={({ history }) => (
+          <CreateContact
+            onCreateContact={(contact) => {
+              this.createContact(contact)
+              history.push('/')
+            }}
+          />
+        )}/>
       </div>
-    );
+    )
   }
 }
 
